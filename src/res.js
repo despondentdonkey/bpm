0a05f4bcad7653d09697911714ff512869954ab8
Parker Miller <despondentdonkey@gmail.com>
2014-05-03 01:37:14 -0600

    Minor changes to res.js

--- src/res.js
+++ src/res.js
@@ -7,14 +7,14 @@ define(function() {
     var texturesToCreate = {};
 
     function loadImages(loader, rootDir, urls) {
-        for (key in urls) {
+        for (var key in urls) {
             var img = loader.loadImage(rootDir + urls[key]);
             texturesToCreate[key] = img;
         }
     }
 
     function createQueuedTextures() {
-        for (key in texturesToCreate) {
+        for (var key in texturesToCreate) {
             result.tex[key] = new PIXI.Texture(new PIXI.BaseTexture(texturesToCreate[key]));
         }
         texturesToCreate = {};
@@ -23,9 +23,9 @@ define(function() {
     function load(onComplete) {
         this.loader = LODE.createLoader();
 
-        loadImages(this.loader, 'res/', {
-            bubble: 'bubble.png',
-            glare: 'bubble-glare.png',
+        loadImages(this.loader, 'res/gfx/', {
+            bubble: 'bubbles/bubble.png',
+            glare: 'bubbles/bubble-glare.png',
         });
 
         this.loader.load(function() {
