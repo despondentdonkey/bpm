ab52260895f4e6b8fdde7dfa2b51fdd7a039f522
dogpantz <dogpantz@gmail.com>
2014-05-06 19:37:25 -0600

    Partially refactored inheritance

--- src/objects.js
+++ src/objects.js
@@ -19,161 +19,165 @@ define(['res', 'gfx', 'input'], function(res, gfx, input) {
             y: 0.5
         };
         this.syncDisplayProperties = true; // If true this will update all display object's position properties (x,y,scale,rotation) to this object's properties.
-    }
 
-    GameObject.prototype._onAdd = function(state) {
-        this.state = state;
-        this.onAdd(state);
-    };
+        this._onAdd = function(state) {
+            this.state = state;
+            this.onAdd(state);
+        };
 
-    GameObject.prototype._onRemove = function(state) {
-        while (this.displayObjects.length > 0) {
-            this.removeDisplay(this.displayObjects[0]);
-        }
-        this.state = null;
-        this.onRemove(state);
-    };
-
-    GameObject.prototype._update = function(delta) {
-        this.update(delta);
-        if (this.syncDisplayProperties) {
-            for (var i=0; i<this.displayObjects.length; ++i) {
-                var obj = this.displayObjects[i];
-                obj.position.x = this.x;
-                obj.position.y = this.y;
-                obj.rotation = -this.angle;
-                obj.anchor.x = this.anchor.x;
-                obj.anchor.y = this.anchor.y;
-                obj.scale.x = this.scale.x;
-                obj.scale.y = this.scale.y;
+        this._onRemove = function(state) {
+            while (this.displayObjects.length > 0) {
+                this.removeDisplay(this.displayObjects[0]);
             }
-        }
-    };
+            this.state = null;
+            this.onRemove(state);
+        };
 
-    GameObject.prototype.addDisplay = function(display, container) {
-        this.displayObjects.push(display);
-        return this.state.addDisplay(display, container);
-    };
-
-    GameObject.prototype.removeDisplay = function(display) {
-        this.displayObjects.splice(this.displayObjects.indexOf(display), 1);
-        return this.state.removeDisplay(display);
-    };
-
-    GameObject.prototype.addId = function(id) {
-        if (typeof id === 'string') {
-            if (!this.hasId(id)) {
-                this.idList.push(id);
+        this._update = function(delta) {
+            this.update(delta);
+            if (this.syncDisplayProperties) {
+                for (var i=0; i<this.displayObjects.length; ++i) {
+                    var obj = this.displayObjects[i];
+                    obj.position.x = this.x;
+                    obj.position.y = this.y;
+                    obj.rotation = -this.angle;
+                    obj.anchor.x = this.anchor.x;
+                    obj.anchor.y = this.anchor.y;
+                    obj.scale.x = this.scale.x;
+                    obj.scale.y = this.scale.y;
+                }
             }
-        } else if (id instanceof Array) {
-            for (var i=0; i<id.length; ++i) {
-                this.addId(id[i]);
-            }
-        }
-    };
+        };
 
-    GameObject.prototype.hasId = function(id) {
-        if (typeof id === 'string') {
-            for (var i=0; i<this.idList.length; ++i) {
-                if (this.idList[i] === id) {
+        this.addDisplay = function(display, container) {
+            this.displayObjects.push(display);
+            return this.state.addDisplay(display, container);
+        };
+
+        this.removeDisplay = function(display) {
+            this.displayObjects.splice(this.displayObjects.indexOf(display), 1);
+            return this.state.removeDisplay(display);
+        };
+
+        this.addId = function(id) {
+            if (typeof id === 'string') {
+                if (!this.hasId(id)) {
+                    this.idList.push(id);
+                }
+            } else if (id instanceof Array) {
+                for (var i=0; i<id.length; ++i) {
+                    this.addId(id[i]);
+                }
+            }
+        };
+
+        this.hasId = function(id) {
+            if (typeof id === 'string') {
+                for (var i=0; i<this.idList.length; ++i) {
+                    if (this.idList[i] === id) {
+                        return true;
+                    }
+                }
+            } else if (id instanceof Array) {
+                for (var i=0; i<id.length; ++i) {
+                    if (!this.hasId(id[i])) {
+                        return false;
+                    }
+                }
+
+                if (id.length >= 1) {
                     return true;
                 }
             }
-        } else if (id instanceof Array) {
-            for (var i=0; i<id.length; ++i) {
-                if (!this.hasId(id[i])) {
-                    return false;
-                }
-            }
-
-            if (id.length >= 1) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    GameObject.prototype.removeId = function(id) {
-        if (typeof id === 'string') {
-            if (this.hasId(id)) {
-                this.idList.splice(this.idList.indexOf(id), 1);
-            }
-        } else if (id instanceof Array) {
-            for (var i=0; i<id.length; ++i) {
-                this.removeId(id[i]);
-            }
-        }
-    };
-
-    GameObject.prototype.getBounds = function() {
-        var width = this.width * this.anchor.x;
-        var height = this.height * this.anchor.y;
-        return {
-            x1: this.x - width,
-            y1: this.y - height,
-            x2: this.x + width,
-            y2: this.y + height,
+            return false;
         };
-    };
 
-    GameObject.prototype.getCollisions = function(id) {
-        var result = [];
-        for (var i=0; i<this.state.objects.length; ++i) {
-            var obj = this.state.objects[i];
-            if (obj !== this && obj.hasId(id)) {
-                var bounds = this.getBounds();
-                var objBounds = obj.getBounds();
-
-                 if (bounds.x1 < objBounds.x2
-                  && bounds.x2 > objBounds.x1
-                  && bounds.y1 < objBounds.y2
-                  && bounds.y2 > objBounds.y1) {
-                    result.push(obj);
+        this.removeId = function(id) {
+            if (typeof id === 'string') {
+                if (this.hasId(id)) {
+                    this.idList.splice(this.idList.indexOf(id), 1);
+                }
+            } else if (id instanceof Array) {
+                for (var i=0; i<id.length; ++i) {
+                    this.removeId(id[i]);
                 }
             }
-        }
-        return result;
-    };
+        };
 
-    GameObject.prototype.onAdd = function(state) {}
-    GameObject.prototype.onRemove = function(state) {}
-    GameObject.prototype.update = function(delta) {};
+        this.getBounds = function() {
+            var width = this.width * this.anchor.x;
+            var height = this.height * this.anchor.y;
+            return {
+                x1: this.x - width,
+                y1: this.y - height,
+                x2: this.x + width,
+                y2: this.y + height,
+            };
+        };
+
+        this.getCollisions = function(id) {
+            var result = [];
+            for (var i=0; i<this.state.objects.length; ++i) {
+                var obj = this.state.objects[i];
+                if (obj !== this && obj.hasId(id)) {
+                    var bounds = this.getBounds();
+                    var objBounds = obj.getBounds();
+
+                     if (bounds.x1 < objBounds.x2
+                      && bounds.x2 > objBounds.x1
+                      && bounds.y1 < objBounds.y2
+                      && bounds.y2 > objBounds.y1) {
+                        result.push(obj);
+                    }
+                }
+            }
+            return result;
+        };
+
+        this.onAdd = function(state) {}
+        this.onRemove = function(state) {}
+        this.update = function(delta) {};
+    }
 
 
-    inherit(BubbleTest, GameObject);
+    //inherit(BubbleTest, GameObject);
+    BubbleTest.prototype = new GameObject();
+    BubbleTest.constructor = BubbleTest;
     function BubbleTest(x, y, tint) {
-        GameObject.call(this);
+        //GameObject.call(this);
         this.initialX = x; this.initialY = y;
         this.xmod=0; this.ymod=0;
         this.offsetX = 0; this.offsetY = 0;
         this.tint = tint;
+
+        this.onAdd = function(state) {
+            // BubbleTest render test
+            var bub = new gfx.pixi.Sprite(res.tex.bubble);
+            this.addDisplay(bub, state.batch);
+        };
+
+        this.update = function(delta) {
+            this.xmod += 0.02;
+            this.ymod += 0.01;
+
+            this.offsetX = Math.cos(this.xmod) * 20;
+            this.offsetY = Math.sin(this.ymod) * 20;
+
+            this.x = this.initialX + this.offsetX;
+            this.y = this.initialY + this.offsetY;
+
+            this.angle = Math.tan(this.xmod/3);
+            this.scale.x = Math.cos(this.xmod) * 1.5;
+            this.scale.y = Math.sin(this.ymod) * 1.5;
+        };
     }
 
-    BubbleTest.prototype.onAdd = function(state) {
-        // BubbleTest render test
-        var bub = new gfx.pixi.Sprite(res.tex.bubble);
-        this.addDisplay(bub, state.batch);
-    };
 
-    BubbleTest.prototype.update = function(delta) {
-        this.xmod += 0.02;
-        this.ymod += 0.01;
-
-        this.offsetX = Math.cos(this.xmod) * 20;
-        this.offsetY = Math.sin(this.ymod) * 20;
-
-        this.x = this.initialX + this.offsetX;
-        this.y = this.initialY + this.offsetY;
-
-        this.angle = Math.tan(this.xmod/3);
-        this.scale.x = Math.cos(this.xmod) * 1.5;
-        this.scale.y = Math.sin(this.ymod) * 1.5;
-    };
-
-
-    inherit(PinTest, GameObject);
+    //inherit(PinTest, GameObject);
+    PinTest.prototype = new GameObject();
+    PinTest.constructor = PinTest;
     function PinTest(x, y, angle) {
-        GameObject.call(this);
+        //GameObject.call(this);
         this.x = x; this.y = y;
         this.width = 13; this.height = 12;
         this.angle = angle;
