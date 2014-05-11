86032dc5915817337ad203de91faa335b1bf30d5
Parker Miller <despondentdonkey@gmail.com>
2014-05-10 22:52:13 -0600

    Added life ratio.

--- src/objects.js
+++ src/objects.js
@@ -186,7 +186,8 @@ define(['bpm', 'res', 'gfx', 'input'], function(bpm, res, gfx, input) {
             this.lifeTimer -= delta;
 
             // y = sqrt(1 -x) + x/4
-            this.graphic.alpha = Math.sqrt(this.lifeTimer / this.lifeTime) + (1-(this.lifeTimer/this.lifeTime))/4;
+            var lifeRatio = this.lifeTimer / this.lifeTime;
+            this.graphic.alpha = Math.sqrt(lifeRatio) + (1-lifeRatio)/4;
 
             if (this.lifeTimer <= 0) {
                 this.state.remove(this);
