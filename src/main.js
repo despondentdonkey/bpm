295bf8166fe77f824523f0ee348e485eb5f9e55d
dogpantz <dogpantz@gmail.com>
2014-05-10 21:04:06 -0600

    Added loop-related functions to main.js, updated depends

--- src/main.js
+++ src/main.js
@@ -6,6 +6,43 @@ requirejs.config({
 
 requirejs(['lib/pixi', 'lib/sfx', 'lib/lode', 'lib/underscore', 'debug', 'utils']);
 
-requirejs(['bpm'], function(bpm) {
-    bpm.run();
+requirejs(['time', 'gfx', 'res', 'states', 'input'], function(time, gfx, res, states, input) {
+    // Override default requestAnimationFrame for maximum compatibility.
+    var requestAnimationFrame = window.requestAnimationFrame
+                           || window.mozRequestAnimationFrame
+                           || window.webkitRequestAnimationFrame
+                           || window.msRequestAnimationFrame
+                           || function(func) { setTimeout(func, 1000/60) };
+
+    function run() {
+        states.setState(new states.Field());
+        console.log('state set');
+        console.log(states.currentState);
+
+        res.load(function() {
+            gfx.init(800, 600, document.getElementById('canvas'));
+            input.init(gfx.renderer.view);
+            dbg.addStateButtons(states);
+            update();
+        });
+    }
+
+    function update() {
+        if (states.currentState) {
+            if (!states.currentStateInit) {
+                states.currentState.init();
+                states.currentStateInit = true;
+            }
+            states.currentState.update(time.delta);
+        }
+
+        input.update();
+        time.update();
+
+        gfx.render();
+
+        requestAnimationFrame(update);
+    }
+
+    run();
 });
