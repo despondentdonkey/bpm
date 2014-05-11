563ff80fa8e54a9ae9eace17dd002eb0ca23bdf5
dogpantz <dogpantz@gmail.com>
2014-05-10 21:02:32 -0600

    Moved all loop and state-related code out of bpm.js
    Moved loop stuff into main, state stuff into states.

--- src/bpm.js
+++ src/bpm.js
@@ -1,54 +1,5 @@
-define(['time', 'gfx', 'res', 'states', 'input'], function(time, gfx, res, states, input) {
-    // Override default requestAnimationFrame for maximum compatibility.
-    var requestAnimationFrame = window.requestAnimationFrame
-                           || window.mozRequestAnimationFrame
-                           || window.webkitRequestAnimationFrame
-                           || window.msRequestAnimationFrame
-                           || function(func) { setTimeout(func, 1000/60) };
-
-    var currentState;
-    var currentStateInit = false;
-
-    function run() {
-        setState(new states.Field());
-
-        var bpm = this;
-        res.load(function() {
-            gfx.init(800, 600, document.getElementById('canvas'));
-            input.init(gfx.renderer.view);
-            dbg.addStateButtons(bpm, states);
-            update();
-        });
-    }
-
-    function update() {
-        if (currentState) {
-            if (!currentStateInit) {
-                currentState.init();
-                currentStateInit = true;
-            }
-            currentState.update(time.delta);
-        }
-
-        input.update();
-        time.update();
-
-        gfx.render();
-
-        requestAnimationFrame(update);
-    }
-
-    function setState(state) {
-        if (currentState) {
-            currentState.destroy();
-        }
-        currentState = state;
-        currentStateInit = false;
-    }
+define(function() {
 
     return {
-        requestAnimationFrame: requestAnimationFrame,
-        run: run,
-        setState: setState,
     };
 });
