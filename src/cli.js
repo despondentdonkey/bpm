bcc205ef5408d2b95e555b486c694ba82f3bd82b
Spencer M <dogpantz@gmail.com>
2014-10-03 00:12:39 -0600

    Fixed bugs with error reporting and various other things
    Cleaned up comments

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,17 @@
 define(['events', 'objects', 'bpm', 'states'], function(events, objects, bpm, states) {
-
-
+    'use-strict';
     // default configurations for constructors
-        // describe what the default command should do if no args are provided
+        // describe what the default command should do if no additional args are provided
         // for example, if 'spawn 10 Bubble' is called, Bubble's armor, x, y, and angle properties need to be defined here
 
     var commandList = {
-        // need to specify all contructor args in opt OR in defaults[objects[obj]] (ie, defaults['Bubble'])
+        // need to specify all contructor args in optStr OR in defaults[objects[obj]] (ie, defaults['Bubble'])
         'spawn': function(amtStr, objStr, optStr) {
+            objStr = capitalize(objStr.toLowerCase());
             var st = states.global.current;
             var amt = Number(amtStr);
-            var opt = optStr !== undefined ? parseObject(optStr);
-            var obj = objects[capitalize(objStr.toLowerCase())];
+            var opt = optStr ? parseObject(optStr) : {};
+            var obj = objects[capitalize(objStr)];
 
             _(opt).defaults(typeof defaults[objStr] === 'object' ? defaults[objStr] : {});
 
@@ -19,7 +19,7 @@ define(['events', 'objects', 'bpm', 'states'], function(events, objects, bpm, st
             if (!st)
                 warnMessages.push('states.global.current is not defined');
             if (typeof opt !== 'object')
-                warnMessages.push('options object is not an object. Error in CLI.js::parseObject or in CLI call')
+                warnMessages.push('options is not an object. Error in CLI.js::parseObject or in CLI call')
             if (!obj instanceof objects.GameObject)
                 warnMessages.push('"'+obj+'" is not defined as a GameObject in objects');
             if (typeof amt !== 'number')
@@ -28,17 +28,21 @@ define(['events', 'objects', 'bpm', 'states'], function(events, objects, bpm, st
                 warnMessages.push(JSON.stringify(opt)+' does not match the argument requirements for the constructor ' + obj.name);
 
             if (warnMessages.length > 0) {
-                getWarning('spawn', arguments, warnMessages)();
+                var warning = getWarning('spawn', _.toArray(arguments), warnMessages);
+                warning();
                 return -1;
             }
-
-
 
             _(amt).times(function() { st.add(new obj.apply(null, opt)); });
         },
     };
 
-    var defaults
+    var defaults = {
+        'Bubble': {
+
+        }
+    };
+
     var parseObject = function(str) {
 
     };
@@ -54,19 +58,20 @@ define(['events', 'objects', 'bpm', 'states'], function(events, objects, bpm, st
             warning.push('\t' + m);
         });
 
-        return _.partial(console.warn, warning.join('\n'));
+        return function() {
+            console.warn(warning.join('\n'));
+        };
     };
 
     // Calls a space delimited command from a command defined in the commandList; format: CLI('<command> <arguments>', this);
-    // CLI.apply or CLI.call to assign a context to commands (enables the use of 'this' in commands)
-    function CLI(commandWithArgs) {
+    function CLI(commandWithArgs, context) {
         var parsed = commandWithArgs.split(' ');
         var command = parsed[0];
         var args = _(parsed).tail();
 
 
         if (typeof commandList[command] === 'function')
-            return commandList[command].apply(this, args);
+            return commandList[command].apply(context, args);
     }
 
     return CLI;
